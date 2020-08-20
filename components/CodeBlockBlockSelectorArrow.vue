ce9b624a960f7753a2e33678381b3b9106c9ea99
hashrock <hashrock@users.noreply.github.com>
2020-08-20 18:10:27 +0900

    feat: add arrow change ui

--- components/CodeBlockBlockSelectorArrow.vue
+++ components/CodeBlockBlockSelectorArrow.vue
@@ -1,11 +1,11 @@
 <template>
   <g>
-    <rect x="0.5" y="0.5" :width="24*3" :height="24" fill="#EEE" stroke="#666" stroke-width="1"></rect>
+    <rect x="0.5" y="0.5" :width="24*2" :height="24" fill="#EEE" stroke="#666" stroke-width="1"></rect>
     <g
       transform="translate(0.5, 0.5)"
       class="typeNav__item"
-      :class="{active: selectedItem.type==='arrow'}"
-      @click="selectedItem.type = 'arrow'"
+      :class="{active: arrow}"
+      @click="onClickArrow"
     >
       <rect x="0" y="0" width="24" height="24" fill="currentcolor"></rect>
       <g transform="translate(20, 12) scale(0.5)">
@@ -16,23 +16,8 @@
     <g
       transform="translate(24.5, 0.5)"
       class="typeNav__item"
-      :class="{active: selectedItem.type==='arrow-both'}"
-      @click="selectedItem.type = 'arrow-both'"
-    >
-      <rect x="0" y="0" width="24" height="24" fill="currentcolor"></rect>
-      <g transform="translate(2, 12) scale(0.3) rotate(180)">
-        <polygon points="0,0 -20,-8 -18,0 -20,8" fill="#333" />
-      </g>
-      <g transform="translate(22, 12) scale(0.3)">
-        <polygon points="0,0 -20,-8 -18,0 -20,8" fill="#333" />
-      </g>
-      <line x1="4" y1="12" x2="20" y2="12" stroke="#333" />
-    </g>
-    <g
-      transform="translate(48.5, 0.5)"
-      class="typeNav__item"
-      :class="{active: selectedItem.type==='line'}"
-      @click="selectedItem.type = 'line'"
+      :class="{active: !arrow}"
+      @click="onClickLine"
     >
       <rect x="0" y="0" width="24" height="24" fill="currentcolor"></rect>
       <line x1="4" y1="12" x2="20" y2="12" stroke="#333" />
@@ -42,7 +27,15 @@
 
 <script>
 export default {
-  props: ["selectedItem"]
+  props: ["arrow"],
+  methods: {
+    onClickArrow() {
+      this.$emit("arrow");
+    },
+    onClickLine() {
+      this.$emit("line");
+    },
+  },
 };
 </script>
 
